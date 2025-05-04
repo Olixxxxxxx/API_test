13f1752da1321060487387743fa7e44d8fc58ada
Olixxxxxxx <chaikovskaiaolga2207@gmail.com>
2025-05-04 21:11:19 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# API_test
+# playwright-typescript-frontend-framework
+
+## Getting started
+
+This is framework for testing UI with typescript and playwright
+
+## Preconditions
+
+Install node and npm
+Instal java for allure report
+
+
+## Run tests
+
+Go to the root of the project and execute commands from terminal:<br/>
+    npm install<br/>
+    npm run test
+
+
+## Alure report
+
+Allure report is created in the folder allure-report
+
+## Playwright report
+
+Playwright report is placed in the folder playwright-report
+
+## Run tests in docker container
+
+docker compose up
+
+Playwright report is placed in the folder playwright-report
+
+For allure report, run first "npx allure generate ./allure-results" and then "npx allure open ./allure-report" after text execution is completed
+
+##  CI run
+
+There is a .gitlab-ci.yml file for runing tests on Gitlab
+
+## Parallel execution
+
+You can control the number of parallel worker processes via the command line or configuration file:
+
+npx playwright test  --project=chromium --workers 2
+
+Configuration file:
+    testConfig = {
+    workers: process.env.CI ? 2 : undefined,
+    };
+
+## Disabling Parallelism
+
+To disable parallelism, set the number of workers to one:
+
+npx playwright test --project=chromium --workers 1
+
+## Run in headless mode
+
+With current settings, test run in headed mode, and headless mode in docker container.
+For running in headless mode on your locale machine set headless on true
--- test/testAPI/registration.cy.js
+++ test/testAPI/registration.cy.js
@@ -1,5 +1,6 @@
 import { UserAPI } from "./userAPI"
 describe('User Registration API', () => {
+    
     before(() => {
         UserAPI.checkApiHealth()
     })
@@ -30,128 +31,118 @@ describe('User Registration API', () => {
             })
         })
     })
-    // it('Register User without Email', () => {
-    //     UserAPI.userData.email = ""
-    //     UserAPI.registerNewUser(
-    //         UserAPI.userData,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Invalid email format')
-    //     })
-    // })
-    // it('Register User with wrong Email', () => {
-    //     UserAPI.userData.email = "xxx.gmail.com"
-    //     UserAPI.registerNewUser(
-    //         UserAPI.userData,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Invalid email format')
-    //     })
-    // })
-    // it('User Registration with Existed Username', () => {
-    //     UserAPI.userData.email = UserAPI.generateUserEmail()
-    //     UserAPI.userData.name = UserAPI.generateUserEmail()
-    //     UserAPI.registerNewUser(
-    //         UserAPI.userData,
-    //         false
-    //     ).then(() => {
-    //         UserAPI.userData.email = UserAPI.generateUserEmail()
-    //         UserAPI.registerNewUser(
-    //             UserAPI.userData,
-    //             false
-    //         ).then((response) => {
-    //                 expect(response.status).eq(400)
-    //                 expect(response.body.error).eq('Username already exists')
-    //             })
-    //     })
-    // })
-    // it('User registration with a username shorter than 5 characters ', () => {
-    //     User.registerNewUser(
-    //         "Olha",
-    //         User.userData.password,
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Username must be between 5 and 8 characters')
-    //     })
-    // })
-    // it('User registration with a username longer than 8 characters ', () => {
-    //     User.registerNewUser(
-    //         "Olha333333",
-    //         User.userData.password,
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Username must be between 5 and 8 characters')
-    //     })
-    // })
-    // it('User registration with password shorter than 6 characters ', () => {
-    //     User.registerNewUser(
-    //         User.userData.username,
-    //         "Oli7!",
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Password must be at least 6 characters long')
-    //     })
-    // })
 
-    // it('User registration with password without spec symbols ', () => {
-    //     User.registerNewUser(
-    //         User.userData.username,
-    //         "Oli789",
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Password must contain at least one special character')
-    //     })
-    // })
+    it('Register User without Email', () => {
+        const testEmail = ""
+        const user = {
+            ...UserAPI.getUserData(),
+            email: testEmail
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Invalid email format')
+        })
+    })
 
-    // it('User registration with password without a uppercase letter ', () => {
-    //     User.registerNewUser(
-    //         User.userData.username,
-    //         "oli789!",
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Password must contain at least one uppercase letter')
-    //     })
-    // })
+    it('Register User with invalid Email', () => {
+        const testEmail = "xxxx.678"
+        const user = {
+            ...UserAPI.getUserData(),
+            email: testEmail
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Invalid email format')
+        })
+    })
 
-    // it('User registration with password without number ', () => {
-    //     User.registerNewUser(
-    //         User.userData.username,
-    //         "Olixxx!",
-    //         User.generateUserEmail(),
-    //         User.userData.firstname,
-    //         User.userData.lastname,
-    //         User.userData.middleName,
-    //         false
-    //     ).then((response) => {
-    //         expect(response.status).eq(400)
-    //         expect(response.body.error).eq('Password must contain at least one number')
-    //     })
-    // })
+    it('User Registration with Existed Username', () => {
+        const testUserName = UserAPI.generateUserName()
+        const user1 = {
+            ...UserAPI.getUserData(),
+            username: testUserName
+        }
+        const user2 = {
+            ...UserAPI.getUserData(),
+            username: testUserName
+        }
+        UserAPI.registerNewUser(user1, true).then(() => {
+            UserAPI.registerNewUser(user2, false).then((response) => {
+                expect(response.status).eq(400)
+                expect(response.body.error).eq('Username already exists')
+            })
+        })
+    })
+
+    it('User registration with a username shorter than 5 characters ', () => {
+        const testUserName = "Jon"
+        const user = {
+            ...UserAPI.getUserData(),
+            username: testUserName
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Username must be between 5 and 8 characters')
+        })
+    })
+
+    it('User registration with a username longer than 8 characters ', () => {
+        const testUserName = "Joni77777777"
+        const user = {
+            ...UserAPI.getUserData(),
+            username: testUserName
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Username must be between 5 and 8 characters')
+        })
+    })
+
+    it('User registration with password shorter than 6 characters', () => {
+        const testPassword = "Xxx5!"
+        const user = {
+            ...UserAPI.getUserData(),
+            password: testPassword
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Password must be at least 6 characters long')
+        })
+    })
+
+    it('User registration with password without spec symbols', () => {
+        const testPassword = "Xxx555"
+        const user = {
+            ...UserAPI.getUserData(),
+            password: testPassword
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Password must contain at least one special character')
+        })
+    })
+
+    it('User registration with password without a uppercase letter', () => {
+        const testPassword = "xxx555!"
+        const user = {
+            ...UserAPI.getUserData(),
+            password: testPassword
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Password must contain at least one uppercase letter')
+        })
+    })
+
+    it('User registration with password without number', () => {
+        const testPassword = "Xxxxxxx!"
+        const user = {
+            ...UserAPI.getUserData(),
+            password: testPassword
+        }
+        UserAPI.registerNewUser(user, false).then((response) => {
+            expect(response.status).eq(400)
+            expect(response.body.error).eq('Password must contain at least one number')
+        })
+    })
 })
